544c48a228dc916a6cc45b3f08950141f349d03a
Ben Chu <benchu118@gmail.com>
2022-01-28 14:57:33 -0600

    ping slash

---
+++ slashcommands/ping.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js")
+const {MessageEmbed} = require("discord.js")
+
+
+const run = async (client, interaction) => {
+    
+    message.reply('Calculating ping...').then((resultMessage) => {
+        const ping = resultMessage.createdTimestamp - message.createdTimestamp
+        const lat = new MessageEmbed()
+        .setColor('#57F287')
+        .setTitle('Pong!\n')
+        .setDescription(`**Bot Latency:** ${ping}_ms_\n\n**API Latency:** ${client.ws.ping}_ms_`)
+        .setTimestamp()
+        .setFooter({ text: 'Lemonade', iconURL: 'https://lemonade.ga/noods/ApplicationFrameHost_ULyyoL2sv1.png' });
+        resultMessage.edit({ embeds: [lat] });
+    })
+}
+
+module.exports = {
+    name: "ping",
+    description: "Ping the bot latency",
+    perm: "",
+    options: [
+    ],
+    run
+}
