2794135f8babb9e47fad4049b3a3cb1b4acc0f7b
Ben Chu <benchu118@gmail.com>
2022-01-27 21:10:29 -0600

    status update

--- events/ready.js
+++ events/ready.js
@@ -3,13 +3,12 @@ module.exports = {
     run: async (bot) => {
         console.log("Logged in as " + bot.client.user.tag)
         setInterval(() => {
-            bot.client.user.setActivity("test", { type: "PLAYING" })
+
             const activities = ["-commands","manga","-commands"]
 
             const randomIndex = Math.floor(Math.random() * (activities.length - 1) + 1);
             const newActivity = activities[randomIndex];
-        
-            bot.client.user.setActivity(newActivity);
+            bot.client.user.setActivity(newActivity, {type: "PLAYING" });
         }, 5000);
     }
 }
